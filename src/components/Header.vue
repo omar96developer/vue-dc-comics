ba62bb2111276b8681d7c0aa5569768422d8dfc0
omar96developer <omakino@hotmail.it>
2021-11-11 18:48:06 +0100

    0.0

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>header</h1>
+</template>
+<script>
+export default {
+    name: "Header"
+    
+}
+
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>main</h1>
+</template>
+<script>
+export default {
+    name: "main"
+    
+}
+
+</script>
