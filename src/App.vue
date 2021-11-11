acfeed0b27c6ede5418b235c9ff7167de131651e
omar96developer <omakino@hotmail.it>
2021-11-11 18:55:06 +0100

    0.0.1

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,11 @@ export default {
 </script>
 
 <style lang="scss">
+@import "style/header";
+@import "style/main";
+@import "style/footer";
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
