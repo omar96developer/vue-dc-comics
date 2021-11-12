abea26c4c5e54884bf66cead1e9951ae67045df5
omar96developer <omakino@hotmail.it>
2021-11-12 01:06:36 +0100

    0.2

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,56 @@
 <template>
-    <h1>main</h1>
+    <main>
+        <section class="main_section1">
+            <div class="container_section1">
+                <h2>{{title}}</h2>
+            </div>
+        </section>
+        <section class="main_section2">
+            <div class="container_section2">
+                 <ul class="list_item">
+                    <li class="nav_item" 
+                    v-for="(item, i) in mainItems" :key="i">
+                        <img :src="item.img" alt="#">                    
+                        <a href="#">{{item.title}}</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </main>
 </template>
 <script>
 export default {
-    name: "main"
+    name: "main",
+    data(){
+        return {
+            title:'-->Content goes here<--',
+             mainItems: [
+                {
+                    img:'../assets/img/buy-comics-digital-comics.png',
+                    title: 'DIGITAL COMICS',
+                    
+                },
+                {
+                    title: 'DC MERCHANDISE',
+                   
+                },
+                {
+                    title: 'SUBSCRIPTION',
+                    
+                },
+                {
+                    title: 'COMIC SHOP LOCATOR',
+                    
+                },
+                {
+                    title: 'DC POWER VISA',
+                    
+                }
+            ],
+            
+
+        }
+    }
     
 }
 
