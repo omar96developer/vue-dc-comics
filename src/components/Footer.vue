65ff9a48d5ca698bcba303e0cca3d3f26cb3cbb4
omar96developer <omakino@hotmail.it>
2021-11-12 23:30:26 +0100

    2.1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,78 @@
 <template>
-    <h1>Footer</h1>
+    <footer>
+        <div class="footer_container_1">
+            <div class="footer_container_A">
+                <section class="footer1">
+
+                    <div class="footer1_list">
+                        <div class="list">
+
+                            <h3>DC COMICS</h3>
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+
+                            <h3>SHOP</h3>
+                            <ul>
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectables</li>
+                                
+                            </ul>
+                        </div>
+                        <div class="list">
+                            <h3>DC</h3>
+                            <ul>
+                                <li>Terms Of Use</li>
+                                <li>Privacy policy (New)</li>
+                                <li>Ad Choices</li>
+                                <li>Advertsing</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPRSC Certificates</li>
+                                <li>Rating</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                                
+                            </ul>
+                        </div>
+                        <div class="list">
+                            <h3>SITES</h3>
+                            <ul>
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="footer1_img">
+                        <div class="footer_logo">
+                            <img src="@/assets/img/dc-logo-bg.png" alt="#">
+                        </div>
+                        
+                    </div>
+                </section>
+            </div>
+        </div>
+        <div class="footer_container_2">
+            <div class="footer_container_B">
+                <section class="footer2">
+                    
+                </section>
+            </div>
+        </div>
+       
+    </footer>
 </template>
 <script>
 export default {
