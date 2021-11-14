cde0cc8e1de65fa9814d0a95ccec3f6483e19eff
omar96developer <omakino@hotmail.it>
2021-11-14 21:52:52 +0100

    3.0

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="product_card" >
+        <img :src="immagine" alt="#">
+        <h4>{{titolo}}</h4>
+        <div class="overlay">
+            <p>Prezzo: {{prezzo}}</p>
+            <p>Tipo: {{tipo}}</p>
+        </div>
+    </div>  
+    
+</template>
+
+<script>
+export default {
+    name: 'ProductCard',
+    props:['immagine', 'titolo', 'prezzo', 'tipo']
+}    
+</script>
